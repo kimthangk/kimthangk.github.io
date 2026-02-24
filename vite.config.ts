d74b301ab4e715044dc6a4b1ee7f497a7e24062d
Thanakorn Angkasirisan <150051980+kimthangk@users.noreply.github.com>
2026-02-25 00:07:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'kimi-plugin-inspect-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
+  base: './',
+  plugins: [inspectAttr(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
